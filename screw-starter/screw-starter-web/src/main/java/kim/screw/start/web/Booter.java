830b3cd5f3011ca4608b6c741f8b0a4168b49777
kongxiangxi <156801554@qq.com>
2019-01-20 23:01:55 +0800

    init

---
+++ screw-starter/screw-starter-web/src/main/java/kim/screw/start/web/Booter.java
@@ -0,0 +1,13 @@
+package kim.screw.start.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booter {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Booter.class, args);
+	}
+	
+}
