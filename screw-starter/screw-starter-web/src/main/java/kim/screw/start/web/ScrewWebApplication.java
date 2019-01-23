237ebaaf8c4d599655eaa520a97511ab4360d149
kongxiangxi <156801554@qq.com>
2019-01-23 23:52:40 +0800

    init2

--- screw-starter/screw-starter-web/src/main/java/kim/screw/start/web/ScrewWebApplication.java
+++ screw-starter/screw-starter-web/src/main/java/kim/screw/start/web/ScrewWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Booter {
+public class ScrewWebApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(Booter.class, args);
+		SpringApplication.run(ScrewWebApplication.class, args);
 	}
 	
 }
